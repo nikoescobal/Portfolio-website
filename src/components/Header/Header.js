1dad282a17e37018d1d7272181a07af57b1e06ea
Niko Escobal <niko.escobal@gmail.com>
2022-02-11 03:19:15 +0800

    Add styled components to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,52 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from "./HeaderStyles";
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href='/'>
+        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+          <DiCssdeck size='3rem'></DiCssdeck> <span>Portfolio</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#tech'>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#about'>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com/nikoescobal'>
+        <AiFillGithub size='3rem'></AiFillGithub>
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com/in/nikolas-escobal/'>
+        <AiFillLinkedin size='3rem'></AiFillLinkedin>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
