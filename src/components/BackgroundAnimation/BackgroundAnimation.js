9a439d9352f2bb47f03cfbeec8d4fe929de001ab
Niko Escobal <niko.escobal@gmail.com>
2022-02-19 04:05:03 +0800

    Change bg animation color

--- src/components/BackgroundAnimation/BackgroundAnimation.js
+++ src/components/BackgroundAnimation/BackgroundAnimation.js
@@ -5,7 +5,7 @@ const BackgroundAnimation = () => (
     <svg
       className='BgAnimation__svg'
       viewBox='0 0 602 602'
-      fill='red'
+      fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
       <g opacity='0.15'>
