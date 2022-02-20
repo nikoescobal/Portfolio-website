ec33fecb839bda42aa6b92df756f5c0b0192d3ba
Niko Escobal <niko.escobal@gmail.com>
2022-02-21 02:52:21 +0800

    Adjust frontend technologies styling

--- src/components/Technologies/TechnologiesBackEndLogos.js
+++ src/components/Technologies/TechnologiesBackEndLogos.js
@@ -3,6 +3,26 @@ import React from "react";
 const TechnologiesBackEndLogos = () => {
   return (
     <div>
+      <a target='_blank' rel='noopener noreferrer' href='https://graphql.org/'>
+        <img
+          alt='Graphql'
+          src='https://img.shields.io/badge/GraphQL-e535ab?style=for-the-badge&logo=graphql&logoColor=white
+
+'
+        />
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://firebase.google.com/'
+      >
+        <img
+          alt='Firebase'
+          src='https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=firebase&logoColor=white
+
+'
+        />
+      </a>
       <a
         target='_blank'
         rel='noopener noreferrer'
@@ -23,22 +43,33 @@ const TechnologiesBackEndLogos = () => {
           src='https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white'
         />
       </a>
-      <a target='_blank' rel='noopener noreferrer' href='https://graphql.org/'>
-        <img
-          alt='Graphql'
-          src='https://img.shields.io/badge/GraphQL-e535ab?style=for-the-badge&logo=graphql&logoColor=white
-
-'
-        />
-      </a>
       <a
         target='_blank'
         rel='noopener noreferrer'
-        href='https://firebase.google.com/'
+        href='https://www.netlify.com/'
       >
         <img
-          alt='Firebase'
-          src='https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=firebase&logoColor=white
+          alt='Netlify'
+          src='https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white
+'
+        />
+      </a>
+      <a target='_blank' rel='noopener noreferrer' href='https://nodejs.org/'>
+        <img
+          alt='Nodejs'
+          src='https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white
+'
+        />
+      </a>
+
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://www.mysql.com/'
+      >
+        <img
+          alt='Mysql'
+          src='https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white
 
 '
         />
@@ -73,37 +104,6 @@ const TechnologiesBackEndLogos = () => {
         <img
           alt='Heroku'
           src='https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white
-'
-        />
-      </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://www.netlify.com/'
-      >
-        <img
-          alt='Netlify'
-          src='https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white
-'
-        />
-      </a>
-      <a target='_blank' rel='noopener noreferrer' href='https://nodejs.org/'>
-        <img
-          alt='Nodejs'
-          src='https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white
-'
-        />
-      </a>
-
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://www.mysql.com/'
-      >
-        <img
-          alt='Mysql'
-          src='https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white
-
 '
         />
       </a>
--- src/components/Technologies/TechnologiesFrontEndLogos.js
+++ src/components/Technologies/TechnologiesFrontEndLogos.js
@@ -3,6 +3,66 @@ import React from "react";
 const TechnologiesFrontEndLogos = () => {
   return (
     <div>
+      <a target='_blank' rel='noopener noreferrer' href='https://reactjs.org/'>
+        <img
+          alt='React'
+          src='https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
+'
+        />
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://tailwindcss.com/'
+      >
+        <img
+          alt='Tailwind'
+          src='https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white
+'
+        />
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://www.javascript.com/'
+      >
+        <img
+          alt='JavaScript'
+          src='https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E'
+        />
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://webpack.js.org/'
+      >
+        <img
+          alt='Webpack'
+          src='https://img.shields.io/badge/Webpack-8ccbf3?style=for-the-badge&logo=webpack&logoColor=white'
+        />
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://getbootstrap.com/'
+      >
+        <img
+          alt='Bootstrap'
+          src='https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
+'
+        />
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://material.io/design'
+      >
+        <img
+          alt='Material Design'
+          src='https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=material-ui&logoColor=white
+'
+        />
+      </a>
       <a
         target='_blank'
         rel='noopener noreferrer'
@@ -35,67 +95,27 @@ const TechnologiesFrontEndLogos = () => {
 '
         />
       </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://getbootstrap.com/'
-      >
+      <a target='_blank' rel='noopener noreferrer' href='https://nextjs.org/'>
         <img
-          alt='Bootstrap'
-          src='https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
+          alt='Next'
+          src='https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white
 '
         />
       </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://material.io/design'
-      >
+      <a target='_blank' rel='noopener noreferrer' href='https://redux.js.org/'>
         <img
-          alt='Material Design'
-          src='https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=material-ui&logoColor=white
+          alt='Redux'
+          src='https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white
 '
         />
       </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://www.javascript.com/'
-      >
+      <a target='_blank' rel='noopener noreferrer' href='https://babeljs.io/'>
         <img
-          alt='JavaScript'
-          src='https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E'
-        />
-      </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://webpack.js.org/'
-      >
-        <img
-          alt='Webpack'
-          src='https://img.shields.io/badge/Webpack-8ccbf3?style=for-the-badge&logo=webpack&logoColor=white'
-        />
-      </a>
-      <a target='_blank' rel='noopener noreferrer' href='https://reactjs.org/'>
-        <img
-          alt='React'
-          src='https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
+          alt='Babel'
+          src='https://img.shields.io/badge/Babel-eccc04?style=for-the-badge&logo=babel&logoColor=white
 '
         />
       </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://tailwindcss.com/'
-      >
-        <img
-          alt='Tailwind'
-          src='https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white
-'
-        />
-      </a>
-
       <a
         target='_blank'
         rel='noopener noreferrer'
@@ -115,27 +135,6 @@ const TechnologiesFrontEndLogos = () => {
         <img
           alt='Styled Components'
           src='https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white
-'
-        />
-      </a>
-      <a target='_blank' rel='noopener noreferrer' href='https://nextjs.org/'>
-        <img
-          alt='Next'
-          src='https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white
-'
-        />
-      </a>
-      <a target='_blank' rel='noopener noreferrer' href='https://redux.js.org/'>
-        <img
-          alt='Redux'
-          src='https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white
-'
-        />
-      </a>
-      <a target='_blank' rel='noopener noreferrer' href='https://babeljs.io/'>
-        <img
-          alt='Babel'
-          src='https://img.shields.io/badge/Babel-eccc04?style=for-the-badge&logo=babel&logoColor=white
 '
         />
       </a>
