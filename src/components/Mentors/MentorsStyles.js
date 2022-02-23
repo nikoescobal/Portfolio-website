13694c714d415a0257290fc57adeed9079853600
Niko Escobal <niko.escobal@gmail.com>
2022-02-24 02:36:27 +0800

    Update grid size

--- src/components/Mentors/MentorsStyles.js
+++ src/components/Mentors/MentorsStyles.js
@@ -15,7 +15,8 @@ export const Boxes = styled.div`
 
   @media ${(props) => props.theme.breakpoints.sm} {
     display: grid;
-    grid-template-columns: repeat(auto-fill, 140px);
+    grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
+    grid-auto-columns: 300px 100px;
     gap: 10px;
     margin: 24px auto;
   }
